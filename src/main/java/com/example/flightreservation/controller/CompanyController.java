f855228d8c85ca1097417f0716cca27e6c1f6298
tegha-romeo <romeotegha@gmail.com>
2025-07-15 16:54:07 +0100

    Add CompanyController to handle API requests for retrieving all companies

---
+++ src/main/java/com/example/flightreservation/controller/CompanyController.java
@@ -0,0 +1,26 @@
+package com.example.flightreservation.controller;
+
+import com.example.flightreservation.model.Company;
+import com.example.flightreservation.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/companies")
+public class CompanyController {
+    private final CompanyRepository companyRepository;
+
+    @Autowired
+    public CompanyController(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @GetMapping
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+}
